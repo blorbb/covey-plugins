359241d59e6abe8f549c767dce8b6dbc501d3482
blorbb <88137137+blorbb@users.noreply.github.com>
2024-11-10 13:54:16 +1100

    use constructor for app switcher

--- app-switcher/src/lib.rs
+++ app-switcher/src/lib.rs
@@ -6,82 +6,78 @@ use qpmu_api::{
     host, register, Action, Capture, ListItem, Plugin, QueryResult, Weights,
 };
 
-const USELESS_CATEGORIES: [&str; 7] = [
-    "System",
-    "Development",
-    "Qt",
-    "KDE",
-    "GNOME",
-    "GTK",
-    "Application",
-];
+struct AppSwitcher {
+    entries: Vec<ListItem>,
+}
 
-static ENTRIES: LazyLock<Vec<ListItem>> = LazyLock::new(|| {
-    let Ok(entries) = host::read_dir("/usr/share/applications") else {
-        return vec![];
-    };
+impl Plugin for AppSwitcher {
+    fn new(_: String) -> Result<Self> {
+        let entries = host::read_dir("/usr/share/applications")?
+            .into_iter()
+            .map(PathBuf::from)
+            .filter_map(|entry| {
+                Some((
+                    entry.file_stem()?.to_str()?.to_string(),
+                    desktop::Entry::parse(host::read_file(entry).ok()?).ok()?,
+                ))
+            })
+            .filter(|(_, entry)| {
+                let entry = entry.section("Desktop Entry");
 
-    entries
-        .into_iter()
-        .map(PathBuf::from)
-        .filter_map(|entry| {
-            Some((
-                entry.file_stem()?.to_str()?.to_string(),
-                desktop::Entry::parse(host::read_file(entry).ok()?).ok()?,
-            ))
-        })
-        .filter(|(_, entry)| {
-            // NoDisplay=true desktop entries aren't for user use.
-            entry.section("Desktop Entry").attr("NoDisplay") != Some("true")
-            // if there are no icons then it's probably not a user app.
-                && entry.section("Desktop Entry").has_attr("Icon")
-            // filter out based on categories
-                && entry
-                    .section("Desktop Entry")
-                    .attr("Categories")
-                    .is_some_and(|cats| {
+                // NoDisplay=true desktop entries aren't for user use.
+                // if there are no icons then it's probably not a user app.
+                entry.attr("NoDisplay") != Some("true")
+                    && entry.has_attr("Icon")
+                    && entry.attr("Categories").is_some_and(|cats| {
                         cats.split_terminator(';')
                             .filter(|cat| !USELESS_CATEGORIES.contains(cat))
                             .count()
                             > 0
                     })
-        })
-        .filter_map(|(file_stem, entry)| {
-            let metadata = format!(
-                "{}\n{}",
-                entry
-                    .section("Desktop Entry")
-                    .attr("Exec")?
-                    .replace("%u", "")
-                    .replace("%U", ""),
-                entry
-                    .section("Desktop Entry")
-                    .attr("StartupWMClass")
-                    .unwrap_or(&file_stem)
-            );
+            })
+            .filter_map(|(file_stem, entry)| {
+                let entry = entry.section("Desktop Entry");
+                let metadata = format!(
+                    "{}\n{}",
+                    entry.attr("Exec")?.replace("%u", "").replace("%U", ""),
+                    entry.attr("StartupWMClass").unwrap_or(&file_stem)
+                );
 
-            Some(
-                ListItem::new(entry.section("Desktop Entry").attr("Name")?)
-                    .with_description(
-                        entry
-                            .section("Desktop Entry")
-                            .attr("Comment")
-                            .unwrap_or_default(),
-                    )
-                    .with_metadata(metadata)
-                    .with_icon(entry.section("Desktop Entry").attr("Icon")),
-            )
-        })
-        .collect()
-});
+                Some(
+                    ListItem::new(entry.attr("Name")?)
+                        .with_description(entry.attr("Comment").unwrap_or_default())
+                        .with_metadata(metadata)
+                        .with_icon(entry.attr("Icon")),
+                )
+            })
+            .collect();
 
-static KDOTOOL_PATH: LazyLock<String> = LazyLock::new(|| {
-    let mut s = std::env::var("HOME").expect("HOME variable must be set");
-    s.push_str("/.cargo/bin/kdotool");
-    s
-});
+        Ok(Self { entries })
+    }
 
-struct AppSwitcher;
+    fn query(&mut self, query: String) -> Result<QueryResult> {
+        Ok(QueryResult::SetList(host::rank(
+            &query,
+            &self.entries,
+            Weights::default(),
+        )))
+    }
+
+    fn activate(&mut self, selected: ListItem) -> Result<impl IntoIterator<Item = Action>> {
+        let (exec_cmd, class) = selected.metadata.split_once('\n').unwrap();
+
+        if !class.is_empty() {
+            // try and activate it with kdotool
+            if activate_kdotool(class).is_ok() {
+                return Ok(vec![Action::Close]);
+            }
+        }
+
+        Ok(vec![Action::Close, Action::RunShell(exec_cmd.to_string())])
+    }
+}
+
+register!(AppSwitcher);
 
 /// Tries to open the window using kdotool, returning None if it fails.
 fn activate_kdotool(class: &str) -> Result<()> {
@@ -106,31 +102,18 @@ fn activate_kdotool(class: &str) -> Result<()> {
     Ok(())
 }
 
-impl Plugin for AppSwitcher {
-    fn new(_: String) -> Result<Self> {
-        Ok(Self)
-    }
+const USELESS_CATEGORIES: [&str; 7] = [
+    "System",
+    "Development",
+    "Qt",
+    "KDE",
+    "GNOME",
+    "GTK",
+    "Application",
+];
 
-    fn query(&mut self, query: String) -> Result<QueryResult> {
-        Ok(QueryResult::SetList(host::rank(
-            &query,
-            &*ENTRIES,
-            Weights::default(),
-        )))
-    }
-
-    fn activate(&mut self, selected: ListItem) -> Result<impl IntoIterator<Item = Action>> {
-        let (exec_cmd, class) = selected.metadata.split_once('\n').unwrap();
-
-        if !class.is_empty() {
-            // try and activate it with kdotool
-            if activate_kdotool(class).is_ok() {
-                return Ok(vec![Action::Close]);
-            }
-        }
-
-        Ok(vec![Action::Close, Action::RunShell(exec_cmd.to_string())])
-    }
-}
-
-register!(AppSwitcher);
+static KDOTOOL_PATH: LazyLock<String> = LazyLock::new(|| {
+    let mut s = std::env::var("HOME").expect("HOME variable must be set");
+    s.push_str("/.cargo/bin/kdotool");
+    s
+});
