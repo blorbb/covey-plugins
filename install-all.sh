a8c882bdc773b65cc7b94e495ea0a76719d22b0f
blorbb <88137137+blorbb@users.noreply.github.com>
2024-11-22 22:46:40 +1100

    Add debug option to install script

--- install-all.sh
+++ install-all.sh
@@ -5,6 +5,10 @@ for directory in ./*/ ; do
     if [[ ! "$directory" =~ "target" ]]; then
         echo "installing $directory"
         # parens required to enter subshell so that each cd starts from here
-        (cd "$directory" && cargo build --release --artifact-dir ~/.config/qpmu/plugins -Z unstable-options)
+        if [[ "$1" = "debug" ]]; then
+            (cd "$directory" && cargo build --artifact-dir ~/.config/qpmu/plugins -Z unstable-options)
+        else
+            (cd "$directory" && cargo build --release --artifact-dir ~/.config/qpmu/plugins -Z unstable-options)
+        fi
     fi
 done
