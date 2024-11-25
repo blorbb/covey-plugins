e0e168a18e15d6bc467b472a1d416e1b890b4ab2
blorbb <88137137+blorbb@users.noreply.github.com>
2024-11-25 20:25:53 +1100

    Update install script to new plugin location

--- install-all.sh
+++ install-all.sh
@@ -1,14 +1,15 @@
 #!/usr/bin/env sh
 
-for directory in ./*/ ; do
+for directory in */ ; do
+    directory=${directory%*/}  # remove the trailing "/"
     # directory does not contain "target"
     if [[ ! "$directory" =~ "target" ]]; then
         echo "installing $directory"
         # parens required to enter subshell so that each cd starts from here
         if [[ "$1" = "debug" ]]; then
-            (cd "$directory" && cargo build --artifact-dir ~/.config/qpmu/plugins -Z unstable-options)
+            (cd "$directory" && cargo build --artifact-dir ~/.local/share/qpmu/plugins/$directory -Z unstable-options)
         else
-            (cd "$directory" && cargo build --release --artifact-dir ~/.config/qpmu/plugins -Z unstable-options)
+            (cd "$directory" && cargo build --release --artifact-dir ~/.local/share/qpmu/plugins/$directory -Z unstable-options)
         fi
     fi
 done
